a158c35c1268ef517e23db25aaf17d6921975cca
manhdd <manhdd@runsystem.net>
2017-03-24 09:01:22 +0700

    videos_list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyDvW1wpGMwO4lgBSFj4vM8-k5k4MG1L6ec';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { videos : []};
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({videos});
+			// this.setState({videos: videos});
+		});
+	}
+	
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+}
 
 ReactDOM.render(
   <App />,
